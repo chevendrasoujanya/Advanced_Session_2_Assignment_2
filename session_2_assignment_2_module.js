83d3f0485cb98c092fee85cfc5b324ff00eac21a
chevendrasoujanya <kesar989@gmail.com>
2017-01-06 15:55:01 +0530

    Add files via upload

---
+++ session_2_assignment_2_module.js
@@ -0,0 +1,16 @@
+// export const sqrt = Math.sqrt;
+export function addition(x, y) {
+    return x + x;
+}
+
+export function subtraction(x, y) {
+    return x - y;
+}
+
+export function multiplication(x, y) {
+	return x * y;
+}
+
+export function square(x) {
+	return x * x;
+}
